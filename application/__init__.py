c06e9bbcf5e14ee84911a4679158b238ebe925d4
bolawole <dewoleawe@gmail.com>
2020-08-10 20:41:25 -0400

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "MYSERETKEYLOL"
-app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:BOLAwole_1995@localhost/flack"
+app.config['SQLALCHEMY_DATABASE_URI']="insert your database url here"
 socketio = SocketIO(app)
 db=SQLAlchemy(app)
 login_manager=LoginManager(app)
